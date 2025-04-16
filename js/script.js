399de16e7b7f7e4a4e81181d6f250dde5c368592
jpfurtado <jpfurtado@gmail.com>
2025-04-16 20:22:49 -0300

    AULA 16/04/2025

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ teste = 'Jose';
 console.log(typeof(teste));
 document.write("<hr> O valor da variável teste é " + teste + "<hr>");
 console.log(teste.toUpperCase());
+
+var lista = ['Arroz','Feijão','Carne','Macarrão'];
+var listaUl = document.createElement('ul');
+var body = document.getElementsByTagName('body');
+
+body[0].appendChild(listaUl);
+
+for (var i=0;i<lista.length;i++) {
+    var listaFor = document.createElement('li');
+    var textoLi = document.createTextNode(lista[i]);
+    listaFor.appendChild(textoLi);
+    listaUl.appendChild(listaFor);
+}
+
